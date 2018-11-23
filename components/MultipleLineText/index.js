5d3fa3e3e28103adf6770cf3e518a3f6c1aee16f
孙海燕 <15358466693@163.com>
2018-11-23 17:10:12 +0800

    优化multipleLineText

--- components/MultipleLineText/index.js
+++ components/MultipleLineText/index.js
@@ -9,33 +9,50 @@ export default class MultipleLineText extends PureComponent {
         this.state = {
             height: 'auto',
             needMoreBtn: false,
-            showEllipsis: true
+            showEllipsis: true,
+            clientHeight: 'auto'
         }
     }
+
     componentDidMount() {
+
+        this.init()
+
+        window.onresize = () => {
+            clearTimeout(this.initTimer)
+            this.initTimer = setTimeout(() => {
+                this.init()
+            }, 500)
+        }
+    }
+
+    init() {
         const {textLine = 2} = this.props
-        let clientHeight = this.textContainer.clientHeight
-        let clientWidth = this.textContainer.clientWidth
+
+        let clientHeight = this.realText.clientHeight
         let singleLineHeight = this.testText.clientHeight
-        let singleTextWidth = this.testText.clientWidth
-        let ellipsisBtnWidth = this.ellipsisBtn.offsetWidth
-
         let height = singleLineHeight * textLine
-        let fontNum = parseInt(clientWidth / singleTextWidth) * textLine
-        this.originalText = this.text.innerText.toString()
-        let ellipsisFontNum = Math.ceil(ellipsisBtnWidth / singleTextWidth)
-        let displayFontNum = fontNum - ellipsisFontNum - 2
-        this.displayText = this.originalText.slice(0, displayFontNum) + '...'
-
-        this.text.innerText = this.displayText
 
         let needMoreBtn = clientHeight > (height + 5)
+
+        if(needMoreBtn) {
+            let clientWidth = this.realText.clientWidth
+            let singleTextWidth = this.testText.clientWidth
+            let ellipsisBtnWidth = this.ellipsisBtn.offsetWidth
+
+            let fontNum = parseInt(clientWidth / singleTextWidth) * textLine
+            this.originalText = this.originalText || this.text.innerText.toString()
+            let ellipsisFontNum = Math.ceil(ellipsisBtnWidth / singleTextWidth)
+            let displayFontNum = fontNum - ellipsisFontNum - 2
+            this.displayText = this.originalText.slice(0, displayFontNum) + '...'
+            this.text.innerText = this.displayText
+        }
 
         this.setState({
             needMoreBtn: needMoreBtn,
             showEllipsis: needMoreBtn,
             height: height,
-            offsetHeight: clientHeight
+            clientHeight: clientHeight
         })
     }
 
@@ -52,13 +69,13 @@ export default class MultipleLineText extends PureComponent {
     }
 
     expandText() {
-        const {offsetHeight} = this.state
+        const {clientHeight} = this.state
 
         this.text.innerText = this.originalText
         this.setState({
             showEllipsis: false
         })
-        this.textContainer.style.height = offsetHeight + 'px'
+        this.textContainer.style.height = clientHeight + 'px'
     }
 
     render() {
@@ -71,7 +88,22 @@ export default class MultipleLineText extends PureComponent {
                 ref={(ref) => this.textContainer=ref}
             >
                 <span ref={ref => this.text = ref}>{children}</span>
-                {needMoreBtn && !showEllipsis && <span 
+                <span 
+                    style={{position: 'absolute' ,zIndex: -9, left: 0, top: 0, visibility: 'hidden'}}
+                    ref={ref => this.realText=ref}
+                >
+                    {children}
+                    <a className="ml-ellipsis-btn">
+                        <span>收起</span>
+                        <Icon 
+                            name="chevron-up" 
+                            size="xxs" 
+                            color="#24A8E8" 
+                            style={{marginLeft: 5}}
+                        />
+                    </a>
+                </span>
+                {needMoreBtn && !showEllipsis && <a 
                     className="ml-ellipsis-btn" 
                     
                     onClick={this.collapseText.bind(this)}
@@ -83,14 +115,13 @@ export default class MultipleLineText extends PureComponent {
                         color="#24A8E8" 
                         style={{marginLeft: 5}}
                     />
-                </span>}
+                </a>}
             {showEllipsis && <span 
                 className={`ml-more-container ${ellipsiClassName || ''}`} 
                 ref={ref => this.ellipsisBtn = ref}
                 style={ellipsisStyle}
-                onClick={this.expandText.bind(this)}
             >
-                <a className="ml-ellipsis-btn">
+                <a className="ml-ellipsis-btn" onClick={this.expandText.bind(this)}>
                     <span>更多</span>
                     <Icon 
                         name="chevron-down" 
